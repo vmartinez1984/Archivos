2afc31d2d3743a9b3a5c093cc920005a37d4965d
victor bravo <ahal_tocob@hotmail.com>
2022-03-19 13:26:05 -0600

    Commit

---
+++ Files/Models/Archive.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Files.Models
+{
+    public class Archive : BaseEntity
+    {        
+        [StringLength(150)]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+
+        [Display(Name = "Archivo")]
+        public string Base64 { get; set; }
+
+        [StringLength(10)]
+        public string Extension { get; set; }
+
+        [ForeignKey(nameof(Folder))]
+        [Display(Name = "Carpeta")]
+        [Required(ErrorMessage = "Seleccione una carpeta")]
+        public int FolderId { get; set; }
+        public Folder Folder { get; set; }
+
+        [Required]
+        [Display(Name = "Solo lectura")]
+        public bool OnlyRead { get; set; }
+
+        [Required(ErrorMessage = "Agregue un archivo")]
+        [NotMapped]
+        [Display(Name = "Archivo")]
+        public IFormFile FormFile { get; set; }
+     
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Files/Models/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Files.Models
+{
+    public class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; } = true;
+
+        [Display(Name = "Fecha de registro")]
+        public DateTime DateRegistration { get; set; } = DateTime.Now;
+    }
+}
